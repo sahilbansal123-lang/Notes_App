40493a1297abcc9abf7790ded1f0f84cf08050d9
SAHIL BANSAL <72665907+sahilbansal123-lang@users.noreply.github.com>
2023-03-25 15:45:10 +0530

    Add files via upload

---
+++ src/main/java/com/example/a6notesapp/Models/NoteViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.a6notesapp.Models
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.a6notesapp.DataBase.NoteDatabase
+import com.example.a6notesapp.DataBase.NotesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: NotesRepository
+
+    val allnotes : LiveData<List<Note>>
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        repository = NotesRepository(dao)
+        allnotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun insertNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+    fun updateNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+
+}
---
+++ src/main/java/com/example/a6notesapp/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.example.a6notesapp.utilities
+
+const val DATABASE_NAME = "note_database"
